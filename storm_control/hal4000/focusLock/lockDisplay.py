7bebbfd376595ead7ab6f6589125df6181cf645e
Jeffrey Moffitt <jeffrey.moffitt@childrens.harvard.edu>
2026-02-16 13:44:18 -0500

    A temporary snapshot of the working code on M4

--- storm_control/hal4000/focusLock/lockDisplay.py
+++ storm_control/hal4000/focusLock/lockDisplay.py
@@ -361,14 +361,14 @@ class QQPDCamDisplay(QCamDisplay):
         painter.drawRect(0, 0, w, w)
 
         # Draw image.
-        y_start = self.display_pixmap.height()/2 - self.camera_image.height()/2
+        y_start = int(self.display_pixmap.height()/2 - self.camera_image.height()/2)
         destination_rect = QtCore.QRect(0, y_start, self.camera_image.width(), self.camera_image.height())
         painter.drawImage(destination_rect, self.camera_image)
 
         # Draw bounding rectangle.
         if (w != h):
             pen = QtGui.QPen(QtGui.QColor(255,0,0))
-            pen.setWidth(self.display_pixmap.width()/self.width())
+            pen.setWidth(int(self.display_pixmap.width()/self.width()))
             painter.setPen(pen)
             painter.setBrush(QtGui.QColor(0,0,0,0))
             painter.drawRect(destination_rect)
@@ -377,7 +377,7 @@ class QQPDCamDisplay(QCamDisplay):
         if (self.zoom_im_x >= 0):
             self.zoom_image = QtGui.QImage(self.zoom_size, self.zoom_size, QtGui.QImage.Format_RGB32)
             painter = QtGui.QPainter(self.zoom_image)
-            painter.drawPixmap(0, 0, self.display_pixmap, self.zoom_im_x, self.zoom_im_y, self.zoom_size, self.zoom_size)
+            painter.drawPixmap(0, 0, self.display_pixmap, int(self.zoom_im_x), int(self.zoom_im_y), int(self.zoom_size), int(self.zoom_size)) #int wrappers 12-28-24 CARL
         else:
             self.zoom_image = False
 
@@ -461,12 +461,12 @@ class QQPDCamDisplay(QCamDisplay):
             if self.adjust_mode:
 
                 painter.setPen(QtGui.QColor(100,100,100))
-                painter.drawLine(0.0, 0.5*self.height(), self.width(), 0.5*self.height())
+                painter.drawLine(0, int(0.5*self.height()), int(self.width()), int(0.5*self.height())) #int wrappers 12-28-24 CARL
                 for mult in [0.25, 0.5, 0.75]:
-                    painter.drawLine(mult*self.width(), 0.0, mult*self.width(), self.height())
+                    painter.drawLine(int(mult*self.width()), 0, int(mult*self.width()), int(self.height())) #int wrappers 12-28-24 CARL
 
                 if self.zoom_image:
-                    destination_rect = QtCore.QRect(self.zoom_x, self.zoom_y, self.zoom_size, self.zoom_size)
+                    destination_rect = QtCore.QRect(int(self.zoom_x), int(self.zoom_y), int(self.zoom_size), int(self.zoom_size)) #int wrappers 12-28-24 CARL
                     painter.drawImage(destination_rect, self.zoom_image)
                     painter.setPen(QtGui.QColor(200,200,200))
                     painter.setBrush(QtGui.QColor(0,0,0,0))
